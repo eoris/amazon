3fa119d52c5096d0b60b2fe4b84d0671e9b6441e
eoris <eoris.work@gmail.com>
2016-01-12 15:55:10 +0200

    init books controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,6 @@ class Book < ActiveRecord::Base
   has_and_belongs_to_many :authors
 
   validates :title, :price, :quantity_in_stock, presence: true
-  validates :price, numericality: true
-  validates :quantity_in_stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates_numericality_of :price
+  validates_numericality_of :quantity_in_stock, greater_than_or_equal_to: 0
 end
--- db/migrate/20151225121720_create_books.rb
+++ db/migrate/20151225121720_create_books.rb
@@ -5,7 +5,6 @@ class CreateBooks < ActiveRecord::Migration
       t.text :descirption
       t.decimal :price, precision: 9, scale: 2, null: false
       t.integer :quantity_in_stock, null: false
-      t.integer :category_id, foreign_key: true
       t.belongs_to :category
 
       t.timestamps null: false
