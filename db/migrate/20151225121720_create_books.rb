2607c3868c964805e4ed0d7cb83f8be102de693f
eoris <eoris.work@gmail.com>
2016-01-14 16:01:30 +0200

    bootstraped and some fix

--- db/migrate/20151225121720_create_books.rb
+++ db/migrate/20151225121720_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
       t.string :title, index: true, null: false
-      t.text :descirption
+      t.text :description
       t.decimal :price, precision: 9, scale: 2, null: false
       t.integer :quantity_in_stock, null: false
       t.belongs_to :category
