8f2cc8b6a09594441d6b0e7400e49af31a8ea4fc
eoris <eoris.work@gmail.com>
2016-01-18 10:28:58 +0200

    new models tests

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  let(:rating) { FactoryGirl.create :rating }
+
+  it { is_expected.to have_db_column(:review) }
+  it { is_expected.to have_db_column(:rating) }
+  it { is_expected.to have_db_column(:customer_id) }
+  it { is_expected.to have_db_column(:book_id) }
 
   it { is_expected.to belong_to(:book) }
   it { is_expected.to belong_to(:customer) }
+
+  it { is_expected.to validate_numericality_of(:rating).is_less_than_or_equal_to(10) }
+  it { is_expected.to validate_numericality_of(:rating).is_greater_than_or_equal_to(0) }
+
 
 end
