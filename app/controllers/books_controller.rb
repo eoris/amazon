3cdaa8bd4c864413252807e5bcb58bbe97ad4bf5
eoris <eoris.work@gmail.com>
2016-01-19 16:45:45 +0200

    authors fullname in books view and I18n init

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.first
+    @book = Book.find_by(id: 14)
+    @authors = @book.authors.map {|n| "#{n.firstname} #{n.lastname}" }.to_sentence
   end
 end
