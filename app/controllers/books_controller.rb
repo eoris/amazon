c3486f9c560e4196d47949ba402aa43b19b0ecb7
eoris <eoris.work@gmail.com>
2016-01-20 09:54:04 +0200

    authors fullname to bookhelper, nobooksfound on _book view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,5 @@
 class BooksController < ApplicationController
   def index
     @book = Book.find_by(id: 14)
-    @authors = @book.authors.map {|n| "#{n.firstname} #{n.lastname}" }.to_sentence
   end
 end
--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,7 @@
 module BooksHelper
+
+  def authors_fullname
+    @book.authors.map {|n| "#{n.firstname} #{n.lastname}" }.to_sentence
+  end
+
 end
