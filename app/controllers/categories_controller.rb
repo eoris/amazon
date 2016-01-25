79f9c301ced6cfcd1cac7fa564ffdcee59d14bfa
eoris <eoris.work@gmail.com>
2016-01-25 12:53:00 +0200

    books partial and navigation fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,4 @@
 class CategoriesController < ApplicationController
-  def index
-    @books = Book.page(params[:page]).per(9)
-    @categories = Category.all
-  end
-
   def show
     @category = Category.find(params[:id])
     @categories = Category.all
