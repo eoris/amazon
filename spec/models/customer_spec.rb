f75217e8e6ef0977dad5947b1327459df8e0847f
eoris <eoris.work@gmail.com>
2016-01-29 10:27:40 +0200

    customer spec fix

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,11 +6,17 @@ RSpec.describe Customer, type: :model do
   it { is_expected.to have_db_column(:firstname) }
   it { is_expected.to have_db_column(:lastname) }
   it { is_expected.to have_db_column(:encrypted_password) }
+  it { is_expected.to have_db_column(:reset_password_token) }
+  it { is_expected.to have_db_column(:reset_password_sent_at) }
+  it { is_expected.to have_db_column(:remember_created_at) }
+  it { is_expected.to have_db_column(:sign_in_count) }
+  it { is_expected.to have_db_column(:current_sign_in_at) }
+  it { is_expected.to have_db_column(:last_sign_in_at) }
+  it { is_expected.to have_db_column(:current_sign_in_ip) }
+  it { is_expected.to have_db_column(:last_sign_in_ip) }
 
   it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_presence_of(:firstname) }
-  it { is_expected.to validate_presence_of(:lastname) }
-  it { is_expected.to validate_presence_of(:encrypted_password) }
+  it { is_expected.to validate_presence_of(:password) }
 
   it { is_expected.to have_many(:orders) }
   it { is_expected.to have_many(:ratings) }
