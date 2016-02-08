79766f4282e5d0f32a649e7d8c55fc03bffdd464
eoris <eoris.work@gmail.com>
2016-02-08 14:43:25 +0200

    subtotal order item price

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -9,11 +9,16 @@ class OrderItemsController < ApplicationController
 
   def create
     @order_item = OrderItem.new(order_item_params)
-    @order_item.save
+    @order_item.price = @order_item.subtotal_price(order_item_book)
+    session[:order_item] = @order_item
     redirect_to :back
   end
 
   private
+
+    def order_item_book
+      @book = @order_item.book
+    end
 
     def order_item_params
       params.require(:order_item).permit(:quantity, :book_id)
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,8 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
 
   validates :price, :quantity, presence: true
+
+  def subtotal_price(item)
+    quantity * item.price
+  end
 end
