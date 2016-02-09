f5b3274bd33bb8b1e595da420ffdf92af19d0c45
eoris <eoris.work@gmail.com>
2016-02-09 16:36:42 +0200

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   end
   resources :categories, only: [:index, :show]
   resources :authors, only: [:show]
-  resources :order_items
-  get 'clear_order_items/' => 'order_items#clear'
+  resources :order_items do
+    get :clear, on: :collection
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
