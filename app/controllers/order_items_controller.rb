ef37361ec583ab50f2057f992d5ab6393f293d26
eoris <eoris.work@gmail.com>
2016-02-10 15:46:58 +0200

    book quantity sum

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,10 +1,10 @@
 class OrderItemsController < ApplicationController
   def index
-    @cart = cart
+    @cart = summarized_merge(cart)
   end
 
   def create
-    cart << order_item_params
+    cart << {order_item_params[:book_id] => order_item_params[:quantity]}
     redirect_to :back
   end
 
@@ -14,6 +14,16 @@ class OrderItemsController < ApplicationController
   end
 
   private
+
+    def summarized_merge(cart)
+      hash_2 = Hash.new(0)
+        cart.each do |cart_hash|
+          cart_hash.each do |key, value|
+            hash_2[key] += value.to_i
+          end
+        end
+      hash_2
+    end
 
     def order_item_params
       params.require(:order_item).permit(:quantity, :book_id)
