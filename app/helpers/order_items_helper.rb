0fcb65b279843cec0fc2454a8a7ff7b8e9dc330d
eoris <eoris.work@gmail.com>
2016-02-12 11:35:42 +0200

    cart subtotal and some style fix

--- app/helpers/order_items_helper.rb
+++ app/helpers/order_items_helper.rb
@@ -1,9 +1,24 @@
 module OrderItemsHelper
+
+  def order_item_book(book_id)
+    Book.find_by(id: book_id.to_i)
+  end
+
+  def order_item_price(qty, book_id)
+    qty * order_item_book(book_id).price
+  end
+
   def total_items_quantity
     session[:cart].map {|hash| hash.values}.flatten.map(&:to_i).reduce(:+)
   end
 
   def cart_count
     session[:cart].blank? ? "EMPTY" : total_items_quantity
+  end
+
+  def subtotal(cart)
+    cart.map do |book_id, qty|
+      order_item_price(qty, book_id)
+    end.reduce(:+)
   end
 end
