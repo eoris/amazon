e00d8f6e2cd390d2c5a3d0fd64a0ef0d9abb642e
eoris <eoris.work@gmail.com>
2016-02-27 12:30:05 +0200

    orders show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,16 @@ class OrdersController < ApplicationController
 
   def index
     @orders = @customer.orders
+    @orders_in_progress = @orders.where(state: 'in progress')
+    @orders_in_queue = @orders.where(state: 'in queue')
+  end
+
+  def show
+    begin
+      @order = @customer.orders.find(params[:id])
+    rescue
+      redirect_to orders_path
+    end
   end
 
   private
