6830d532b396d0d56a9d1ceeb2a29ac2be8c0cd1
eoris <eoris.work@gmail.com>
2016-03-09 18:48:37 +0200

    categories controller tests

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
+  let(:category) { create(:category) }
 
+  context "GET #index" do
+    before { get :index }
+
+    it 'render :index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  context 'GET #show' do
+    before { get :show, id: category.id }
+
+    it 'render show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
