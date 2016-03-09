39b2f075768793578c208a7863e6ab7537813f53
eoris <eoris.work@gmail.com>
2016-03-09 12:46:35 +0200

    books controller tests

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
+  let(:book) { create(:book) }
 
+  context "GET #index" do
+    before { get :index }
+
+    it 'render :index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  context "GET #show" do
+    before { get :show, id: book.id }
+
+    it 'render :show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it "assigns book" do
+      expect(assigns(:book)).not_to be_nil
+    end
+  end
+
+  context "GET #bestsellers" do
+    it 'render :bestsellers template' do
+      get :bestsellers
+      expect(response).to render_template(:bestsellers)
+    end
+  end
 end
