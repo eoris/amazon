d58fffe3a0a3b8ef63c5d1071baee6f104f5be89
eoris <eoris.work@gmail.com>
2016-03-09 12:52:09 +0200

    authors controller tests

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe AuthorsController, type: :controller do
+  let(:author) { create(:author) }
 
+  context "GET #show" do
+    before { get :show, id: author.id }
+
+    it 'render :show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it "assigns author" do
+      expect(assigns(:author)).not_to be_nil
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
